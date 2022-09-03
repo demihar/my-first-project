6c3b7f52fd7f1062a187f8c95882935b14888fca
harun.dmr04 <h.demir@campus.tu-berlin.de>
2022-09-03 19:08:34 +0200

    first5 commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(
 app.use("/auth", authRoute);
 
 app.get("/", (req, res) => {
-  res.send("HHHHIIIIIIII")
+  res.send("HI HASEODE!!!!")
 });
 
 app.listen(process.env.PORT || "4040", () => {
